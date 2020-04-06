5c46fd13c67a00a7d1653bf0c78e440be40a97ec
vivianpppp <chunying_su@hotmail.com>
2020-04-06 22:28:00 +0000

    Updating all questions

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 
 int main() {
-  int price_avocado;
-  int price_tomato;
-  int num_avocados;
-  int num_tomatoes;
+  int price_avocado = 3;
+  int price_tomato = 1;
+  int num_avocados = 3;
+  int num_tomatoes = 2;
   // Step 1: Prompt the user to input the number of tomatoes and avocados required.
 
   // Step 2: Prompt the user to input the cost of tomatoes and avocados.
 
   // Step 3: Calculate the total cost.
-
+  int total_cost = num_avocados * price_avocado + num_tomatoes * price_tomato;
   // Step 4: Print out the total cost.
+  cout << "Total cost of guacamole is: $" << total_cost << endl;
 }
