1cc6c2661ef6037a1d6ef76de06ae828004554da
John Tavner <60250176+john-lakeman@users.noreply.github.com>
2020-04-08 03:20:08 +0000

    Updating all questions

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -8,10 +8,19 @@ int main() {
   int num_avocados;
   int num_tomatoes;
   // Step 1: Prompt the user to input the number of tomatoes and avocados required.
+  cout << "What is the number of tomatoes required? " << endl;
+  cin >> num_tomatoes; 
+  cout << "What is the number of avocados required? " << endl;
+  cin >> num_avocados; 
 
   // Step 2: Prompt the user to input the cost of tomatoes and avocados.
-
+  cout << "What is the price of tomatoes? " << endl;
+  cin >> price_tomato; 
+  cout << "What is the price of avocados? " << endl;
+  cin >> price_avocado; 
   // Step 3: Calculate the total cost.
+  int total_cost = (num_tomatoes * price_tomato) + (num_avocados * price_avocado); 
 
   // Step 4: Print out the total cost.
+  cout << "The total cost is: " << total_cost << endl;
 }
--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -7,6 +7,12 @@ int main() {
   string city;
 
   // Step 1. Ask the user for their name and their city.
+  cout << "What is your name, bigshot?" << endl;
+  cin >> name;
+
+  cout << "From where do you hail?" << endl;
+  cin >> city; 
 
   // Step 2. Print the greeting back to the user.
+  cout << "Welcome to " << city << "! Wash your hands, don't touch your face, let's keep " << city << " a COVID-free place." << endl;
 }
