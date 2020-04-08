72203ccb462ef143f1039b886a320797c4cb4bf5
John Tavner <60250176+john-lakeman@users.noreply.github.com>
2020-04-08 02:58:46 +0000

    Updating /home/trex/project/fix-code-0

--- fix-code-0/main.cpp
+++ fix-code-0/main.cpp
@@ -3,10 +3,8 @@
 using namespace std;
 
 int main() {
-  int x=4;
-  int y=5;
-  int z=x*y;
-  cout<<"The value of z is: ";
-  cout<<z;
-  cout<<endl;
+  int x = 4;
+  int y = 5;
+  int z = x * y;
+  cout << "The value of z is: " << z << endl;
 }
