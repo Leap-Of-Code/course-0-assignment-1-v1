ad84c439d6c4d038d06505a5fb710347be4b040d
Harshelite2503 <67180856+Harshelite2503@users.noreply.github.com>
2020-06-21 14:44:52 +0000

    Saving progress for all questions.

--- fix-code-3/main.cpp
+++ fix-code-3/main.cpp
@@ -9,9 +9,9 @@ int main() {
   int total_hours_on_assignments = hours_per_assignment * num_assignments;
 
   // Calculate the total number of lines of code written on assignments.
-  int num_assignments11 = 16;
+  int num_assignments = 16;
   int lines_per_assignment = 45;
-  int total_lines_of_code = num_assignments11 * lines_per_assignment;
+  int total_lines_of_code = num_assignments * lines_per_assignment;
 
   cout << "The total amount of time on assignments is: " << total_hours_on_assignments << endl;
   cout << "The total number of lines of code written is: " << total_lines_of_code << endl;
