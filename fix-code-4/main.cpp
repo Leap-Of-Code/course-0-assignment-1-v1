ff386191267abb3a86c8a024cabddcb6c9560049
Harshelite2503 <67180856+Harshelite2503@users.noreply.github.com>
2020-06-21 15:08:14 +0000

    Saving progress for all questions.

--- fix-code-4/main.cpp
+++ fix-code-4/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
   int hotdogs_eaten = 5;
-  int hamburgers_eaten =2;
+  int hamburgers_eaten = 2;
   int calories_per_hotdog = 100;
   int hamburger_calories = 120;
   int total_calories = hotdogs_eaten * calories_per_hotdog + hamburgers_eaten * hamburger_calories;
