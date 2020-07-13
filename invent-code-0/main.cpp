2345396876cfb19f93abe8c417d8d693fbc4c03f
Harshelite2503 <67180856+Harshelite2503@users.noreply.github.com>
2020-07-13 13:19:54 +0000

    Updating all questions

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -6,14 +6,18 @@ int main() {
   int price_avocado;
   int price_tomato;
   int num_avocados;
-  int num_tomatoes,total_cost;
+  int num_tomatoes;
 
   // Step 1: Prompt the user to input the number of tomatoes and avocados required.
+    cout << "Enter the number of avocados and tomatoes respectively: ";
     cin >> num_avocados >> num_tomatoes;
+
   // Step 2: Prompt the user to input the cost of tomatoes and avocados.
-    cin >> price_tomato >> price_avocado;
+    cout << "Enter the price of avocados and tomatoes respectively: ";
+    cin >> price_avocado >> price_tomato;
   // Step 3: Calculate the total cost.
-     total_cost = num_avocados*price_avocado + num_tomatoes*price_tomato;
+    int total_cost = num_avocados*price_avocado + num_tomatoes*price_tomato;
   // Step 4: Print out the total cost.
-    cout << total_cost;
+
+    cout << "The total cost is: " << total_cost;
 }
