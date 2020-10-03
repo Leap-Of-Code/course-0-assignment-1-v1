cc12a52eacaf4e215fe668c11f2950e3f3a7afc7
anaygupta2004 <55030203+anaygupta2004@users.noreply.github.com>
2020-10-03 16:05:32 +0000

    Saving progress for all questions.

--- fix-code-3/main.cpp
+++ fix-code-3/main.cpp
@@ -6,13 +6,15 @@ int main() {
   // Calculate the total number of hours students spent on assignments.
   int num_assignments = 12;
   int hours_per_assignment = 3;
-  int total_hours_on_assignments = hours_per_assignment * num_assignments;
+  int total_hours_on_assignments;
+  int total_lines_of_code;
+  total_hours_on_assignments = hours_per_assignment * num_assignments;
 
   // Calculate the total number of lines of code written on assignments.
-  int num_assignments = 16;
-  int lines_per_assignment = 45;
-  int total_lines_of_code = num_assignments * lines_per_assignment;
+  num_assignments = 16;
+  hours_per_assignment = 45;
+  total_lines_of_code = num_assignments * hours_per_assignment;
 
-  cout << "The total amount of time on assignments is: " << total_hours_on_assignments << end;
+  cout << "The total amount of time on assignments is: " << total_hours_on_assignments << endl;
   cout << "The total number of lines of code written is: " << total_lines_of_code << endl;
 }
--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 int main() {
   string name;
   string city;
-
+  cout << "What is your name?: ";
+  getline(cin,name);
+  cout << "What is your city?: ";
+  getline(cin, city);
+  cout << "Nice to meet you " << name << " from " << city << ". Have a great day!" << endl;
   // Step 1. Ask the user for their name and their city.
 
   // Step 2. Print the greeting back to the user.
